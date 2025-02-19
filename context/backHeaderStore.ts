93b88483fb02d3f5b4195e5609fcd8c8b3d722ec
yugyusang <yugyusang12@gmail.com>
2025-02-19 15:14:56 +0900

    feat: 헤더바 전역상태 관리를 위한 useBackHeaderStore 구현

---
+++ context/backHeaderStore.ts
@@ -0,0 +1,39 @@
+import { ReactNode } from "react";
+import { create } from "zustand";
+
+type BackHeaderStore = {
+  title: string;
+  setTitle: (title: string) => void;
+  subtitle: string;
+  setSubTitle: (title: string) => void;
+  etcButton: ReactNode;
+  setEtcButton: (etcButton: ReactNode) => void;
+  etcCallback: () => void;
+  setEtcCallback: (etcCallback: () => void) => void;
+};
+
+const useBackHeaderStore = create<BackHeaderStore>((set) => ({
+  title: "페이지 제목",
+  setTitle: (title: string) => set({ title }),
+  subtitle: "페이지 부제목",
+  setSubTitle: (subtitle: string) => set({ subtitle }),
+  etcButton: null,
+  setEtcButton: (etcButton: ReactNode) => set({ etcButton }),
+  etcCallback: () => {},
+  setEtcCallback: (etcCallback: () => void) => set({ etcCallback }),
+}));
+
+export const useBackHeader = () => {
+  const title = useBackHeaderStore((state) => state.title);
+  const setTitle = useBackHeaderStore((state) => state.setTitle);
+
+  const subTitle = useBackHeaderStore((state) => state.subtitle);
+  const setSubTitle = useBackHeaderStore((state) => state.setSubTitle);
+
+  const etcButton = useBackHeaderStore((state) => state.etcButton);
+  const setEtcButton = useBackHeaderStore((state) => state.setEtcButton);
+
+  const etcCallback = useBackHeaderStore((state) => state.etcCallback);
+  const setEtcCallback = useBackHeaderStore((state) => state.setEtcCallback);
+  return { title, setTitle, subtitle: subTitle, setSubTitle, etcButton, setEtcButton, etcCallback, setEtcCallback };
+};
