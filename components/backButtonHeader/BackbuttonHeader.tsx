aca75813cf44e938bfaedef8385f06da7c191213
yugyusang <yugyusang12@gmail.com>
2025-02-20 15:24:35 +0900

    feat: 라우터 상단으로 이동하는 대신 히스토리기반 이전 페이지로 이동하도록 수정

--- components/backButtonHeader/BackbuttonHeader.tsx
+++ components/backButtonHeader/BackbuttonHeader.tsx
@@ -2,11 +2,12 @@
 import { useBackHeader } from "@/context/backHeaderStore";
 import { HTMLAttributes } from "react";
 import BackArrow from "../icons/backArrowIcon.svg";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 type Props = { className?: string } & HTMLAttributes<HTMLDivElement>;
 const BackbuttonHeader = ({ className, ...etc }: Readonly<Props>) => {
   const { title, subtitle, etcButton } = useBackHeader();
+  const router = useRouter();
   return (
     <div
       className={`sticky top-0 z-40 flex w-full flex-row justify-between bg-[#FFFFFFD9] p-[var(--root-layout-margin)] backdrop-blur-[5px] ${
@@ -14,9 +15,14 @@ const BackbuttonHeader = ({ className, ...etc }: Readonly<Props>) => {
       }`}
       {...etc}
     >
-      <Link href="../" className="text-[var(--sub-color)]">
+      <button
+        className="text-[var(--sub-color)]"
+        onClick={() => {
+          router.back();
+        }}
+      >
         <BackArrow />
-      </Link>
+      </button>
       <div className="flex flex-col items-center justify-center gap-2">
         <div className="text-lg font-semibold">{title}</div>
         {subtitle && <div className="text-sm font-normal text-[var(--sub-color)]">{subtitle}</div>}
