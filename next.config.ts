327913c2870d868d080d33714bba35238c352b45
yugyusang <yugyusang12@gmail.com>
2025-02-20 19:48:43 +0900

    chore: 빌드시 문제를 일으키던 svgr 설정을 최신 next.js 설정으로 수정 및 적용

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
 import type { NextConfig } from "next";
 import withSerwistInit from "@serwist/next";
+import { RuleSetRule } from "webpack";
 
 const withSerwist = withSerwistInit({
   swSrc: "sw.ts",
@@ -11,15 +12,30 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["s3-alpha-sig.figma.com"], // 책사진 도메인 허용
   },
-  experimental: {
-    turbo: {
-      rules: {
-        "*.svg": {
-          loaders: ["@svgr/webpack"],
-          as: "*.js",
-        },
-      },
-    },
+  webpack: (config) => {
+    const fileLoaderRule = config.module.rules.find(
+      (rule: RuleSetRule) => rule.test instanceof RegExp && rule.test.test(".svg")
+    );
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/,
+      } as RuleSetRule,
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule?.issuer,
+        resourceQuery: { not: [/url/] },
+        use: ["@svgr/webpack"],
+      } as RuleSetRule
+    );
+
+    if (fileLoaderRule) {
+      fileLoaderRule.exclude = /\.svg$/i;
+    }
+
+    return config;
   },
 };
 
