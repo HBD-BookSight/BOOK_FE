cc88dac8fefd419d7bb0664bc34a590f041d06a5
yugyusang <yugyusang12@gmail.com>
2025-02-21 02:07:43 +0900

    chore: images.domains를 최신설정 속성인 images.remotePatterns 수정 및 @svgr/webpack가 turbo만으로 사용되도록 속성 추가

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,5 @@
 import type { NextConfig } from "next";
 import withSerwistInit from "@serwist/next";
-import { RuleSetRule } from "webpack";
 
 const withSerwist = withSerwistInit({
   swSrc: "sw.ts",
@@ -10,32 +9,31 @@ const withSerwist = withSerwistInit({
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["s3-alpha-sig.figma.com"], // 책사진 도메인 허용
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "s3-alpha-sig.figma.com",
+      },
+    ], // 책사진 도메인 허용
   },
   webpack: (config) => {
-    const fileLoaderRule = config.module.rules.find(
-      (rule: RuleSetRule) => rule.test instanceof RegExp && rule.test.test(".svg")
-    );
-
-    config.module.rules.push(
-      {
-        ...fileLoaderRule,
-        test: /\.svg$/i,
-        resourceQuery: /url/,
-      } as RuleSetRule,
-      {
-        test: /\.svg$/i,
-        issuer: fileLoaderRule?.issuer,
-        resourceQuery: { not: [/url/] },
-        use: ["@svgr/webpack"],
-      } as RuleSetRule
-    );
-
-    if (fileLoaderRule) {
-      fileLoaderRule.exclude = /\.svg$/i;
-    }
+    // SVG 파일에 대한 규칙 추가
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
 
     return config;
+  },
+  experimental: {
+    turbo: {
+      rules: {
+        "*.svg": {
+          loaders: ["@svgr/webpack"],
+          as: "*.js",
+        },
+      },
+    },
   },
 };
 
