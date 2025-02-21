b7bc3a848bd1917886bc8364cf26e0ac785ff6f4
yugyusang <yugyusang12@gmail.com>
2025-02-21 23:02:35 +0900

    feat: 동적라우트를 읽고 헤더를 변경하는 BookHeaderHelper 적용

---
+++ app/(backButtonHeader)/book/[isbn]/BookHeaderHelper.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { useBackHeader } from "@/context/backHeaderStore";
+import { useParams } from "next/navigation";
+import { useEffect } from "react";
+import ShareIcon from "@/components/icons/shareIcon.svg";
+
+const BookHeaderHelper = () => {
+  const { isbn } = useParams();
+  const { setTitle, setEtcButton } = useBackHeader();
+
+  useEffect(() => {
+    if (typeof isbn !== "string") return;
+    setTitle(isbn);
+    setEtcButton(<ShareIcon className="text-[var(--sub-color)]" />);
+    return () => {
+      setTitle("");
+      setEtcButton(null);
+    };
+  }, [setTitle, setEtcButton, isbn]);
+  return null;
+};
+
+export default BookHeaderHelper;
--- app/(backButtonHeader)/book/[isbn]/page.tsx
+++ app/(backButtonHeader)/book/[isbn]/page.tsx
@@ -1,11 +1,15 @@
-"use client";
-import { useBackHeader } from "@/context/backHeaderStore";
-import { useParams } from "next/navigation";
 import React from "react";
+import BookHeaderHelper from "./BookHeaderHelper";
+import BookDetail from "@/components/bookDetail/BookDetail";
 const BookDetailpage = () => {
-  const params = useParams();
-  const {} = useBackHeader();
-  return <main className="flex size-full flex-1 items-center gap-8">{params.isbn}</main>;
+  return (
+    <>
+      <BookHeaderHelper />
+      <main className="flex size-full flex-1 items-center gap-8">
+        <BookDetail />
+      </main>
+    </>
+  );
 };
 
 export default BookDetailpage;
