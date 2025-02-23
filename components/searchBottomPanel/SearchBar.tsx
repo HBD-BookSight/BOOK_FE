d605714178e5d9f299f8ee5364ec80891f6189b1
yugyusang <yugyusang12@gmail.com>
2025-02-23 17:27:17 +0900

    feat: 검색패널이 올라오면 자동으로 검색바가 포커싱되도록 기능 추가

--- components/searchBottomPanel/SearchBar.tsx
+++ components/searchBottomPanel/SearchBar.tsx
@@ -1,16 +1,29 @@
-import { HTMLAttributes } from "react";
+"use client";
+import { HTMLAttributes, useEffect, useRef } from "react";
 import SearchIcon from "../icons/searchIcon.svg";
 
 type Props = { className?: string } & HTMLAttributes<HTMLDivElement>;
 const SearchBar = ({ className, ...props }: Readonly<Props>) => {
+  const SearchBarRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    const focusTimer = setTimeout(() => {
+      //검색패널 올라오는 애니메이션이 끝나기전에 포커싱되면 레이아웃이 문제가 발생하므로 임시조치함
+      SearchBarRef.current?.focus();
+    }, 500);
+
+    return () => clearTimeout(focusTimer);
+  }, []);
+
   return (
-    <div className={`relative w-full bg-white px-[var(--root-layout-margin)] py-3 ${className || ""}`} {...props}>
-      <div className="relative flex w-full flex-row rounded-2xl border p-1">
+    <div className={`sticky top-0 w-full px-[var(--root-layout-margin)] py-3 ${className || ""}`} {...props}>
+      <div className="relative flex w-full flex-row rounded-2xl border bg-white p-1">
         <SearchIcon className="w-11 text-[var(--sub-color)]" />
         <input
-          className="w-full bg-transparent outline-none"
+          className="w-full outline-none"
           placeholder="제목, 작가, 출판사를 검색하세요"
           type="search"
+          ref={SearchBarRef}
         ></input>
       </div>
     </div>
