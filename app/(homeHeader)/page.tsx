9fe91a2da10ce416dd35291524e1c0c9f6d57605
yugyusang <yugyusang12@gmail.com>
2025-02-23 17:26:12 +0900

    feat: 구현된 MainScheduleSlide,PromotionContract적용

--- app/(homeHeader)/page.tsx
+++ app/(homeHeader)/page.tsx
@@ -1,29 +1,13 @@
 import MainBookSlide from "@/components/mainBookSlide/MainBookSlide";
+import MainScheduleSlide from "@/components/mainScheduleSlide/MainScheduleSlide";
+import PromotionContract from "@/components/promotionContract/PromotionContract";
 
 const Home = () => {
   return (
-    <main className="relative flex size-full flex-col items-center">
-      <MainBookSlide />
-      <div>asdf</div>
-      <div>asdf</div>
-      <div>asdf</div>
-      <div>asdf</div>
-      <div>asdf</div>
-      <div>asdf</div>
-      <div>asdf</div>
-      <div>asdf</div>
-      <div>asdf</div>
-      <div>asdf</div>
-      <div>asdf</div>
-      <div>asdf</div>
-      <div>asdf</div>
-      <div>asdf</div>
-      <div>asdf</div>
-      <div>asdf</div>
-      <div>asdf</div>
-      <div>asdf</div>
-      <div>asdf</div>
-      <div>asdf</div>
+    <main className="relative flex size-full flex-col items-center py-5">
+      <MainBookSlide className="mb-12" />
+      <MainScheduleSlide className="mb-5" />
+      <PromotionContract />
     </main>
   );
 };
