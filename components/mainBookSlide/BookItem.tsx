d7cb5de9c9c706cb340a91046af4dc910535a761
yugyusang <yugyusang12@gmail.com>
2025-02-24 15:17:56 +0900

    feat: confetti 효과 추가 및 그에 따른 스타일 수정

--- components/mainBookSlide/BookItem.tsx
+++ components/mainBookSlide/BookItem.tsx
@@ -18,13 +18,13 @@ const BookItem = ({ className, isbn, imageUrl, ind, bookLength, ...props }: Read
         width={ind !== bookLength - 1 ? 194 : 235}
         height={ind !== bookLength - 1 ? 293 : 341}
         className={`absolute rounded-2xl shadow-[5px_0_8px_3px_rgba(0,0,0,0.08)]`}
-        style={{ zIndex: ind }}
+        style={{ zIndex: ind + 1 }}
         priority
       />
       {ind !== bookLength - 1 && (
         <div
           className="pointer-events-none absolute h-[281.91px] w-[194px] rounded-2xl bg-[#FFFFFF99]"
-          style={{ zIndex: ind }}
+          style={{ zIndex: ind + 1 }}
         ></div>
       )}
     </Link>
--- components/mainBookSlide/MainBookSlide.tsx
+++ components/mainBookSlide/MainBookSlide.tsx
@@ -1,7 +1,8 @@
 "use client";
-import React, { HTMLAttributes, useEffect, useState } from "react";
+import React, { HTMLAttributes, useEffect, useRef, useState } from "react";
 import BookItem from "./BookItem";
 import BookDescription from "./BookDescription";
+import ReactConfetti from "react-confetti";
 
 type Props = { className?: string } & HTMLAttributes<HTMLDivElement>;
 
@@ -52,9 +53,36 @@ const MainBookSlide = ({ className, ...etc }: Readonly<Props>) => {
       },
     ]);
   }, []);
+
+  const ref = useRef<HTMLDivElement>(null);
+  const [size, setSize] = useState({ width: 0, height: 0 });
+
+  useEffect(() => {
+    const updateSize = () => {
+      if (ref.current) {
+        setSize({
+          width: ref.current.offsetWidth,
+          height: ref.current.offsetHeight,
+        });
+      }
+    };
+    updateSize();
+    window.addEventListener("resize", updateSize);
+    return () => window.removeEventListener("resize", updateSize);
+  }, []);
+
   return (
-    <section className={`flex size-full flex-col px-[var(--root-layout-margin)] ${className || ""}`} {...etc}>
-      <BookDescription createdAt={new Date()} />
+    <section className={`flex size-full flex-col px-[var(--root-layout-margin)] ${className || ""}`} {...etc} ref={ref}>
+      <ReactConfetti
+        width={size.width}
+        height={size.height}
+        gravity={0.08}
+        friction={0.96}
+        numberOfPieces={size.width / 30}
+        drawShape={drawShape}
+        style={{ zIndex: 0 }}
+      />
+      <BookDescription createdAt={new Date()} className="z-10" />
       <div className="flex size-full h-[341px] flex-row-reverse justify-between pl-[235px]">
         {books.map((book, index) => (
           <BookItem key={index} ind={index} isbn={book.isbn} imageUrl={book.image_url} bookLength={books.length} />
@@ -65,3 +93,12 @@ const MainBookSlide = ({ className, ...etc }: Readonly<Props>) => {
 };
 
 export default MainBookSlide;
+
+const drawShape = (ctx: CanvasRenderingContext2D) => {
+  const path = new Path2D(
+    "M37 2L23 0C23.8 7.2 13.3333 12.3333 8 14C10.5 16.5 17.6 20.9 26 18.5C34.4 16.1 36.8333 6.5 37 2Z"
+  );
+  ctx.save();
+  ctx.fill(path);
+  ctx.restore();
+};
