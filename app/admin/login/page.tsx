5fa35ebef0cbab37aff5e0c60a5525aaccb5a26c
yugyusang <yugyusang12@gmail.com>
2025-02-28 16:29:00 +0900

    feat: 어드민 로그인 페이지 구현

---
+++ app/admin/login/page.tsx
@@ -0,0 +1,11 @@
+import LoginForm from "@/components/loginForm/LoginForm";
+
+const LoginPage = () => {
+  return (
+    <div className="relative flex size-full min-h-[100vh] flex-col items-center justify-center">
+      <LoginForm />
+    </div>
+  );
+};
+
+export default LoginPage;
