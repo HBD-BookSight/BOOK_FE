acb492dff2fea68af5c8a97143d2e1cbb2fde816
yugyusang <yugyusang12@gmail.com>
2025-02-28 16:28:29 +0900

    style: 어드민 페이지 기본 스타일 수정

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -8,9 +8,7 @@ const AdminLayout = ({
 }>) => {
   return (
     <>
-      <body
-        className={`relative mx-auto flex h-full min-h-screen w-screen flex-col bg-white antialiased xl:max-w-[1280px]`}
-      >
+      <body className={`relative mx-auto flex h-full min-h-screen w-screen flex-col bg-white antialiased`}>
         <div className="relative mx-auto flex size-full flex-1 flex-col">{children}</div>
       </body>
     </>
