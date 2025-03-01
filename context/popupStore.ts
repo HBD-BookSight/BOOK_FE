1e851eb7f2c6b081680577df2176483e05bdb753
yugyusang <yugyusang12@gmail.com>
2025-03-01 23:35:06 +0900

    feat: 팝업 생성에 필요한 전역상태 스토어 정의

---
+++ context/popupStore.ts
@@ -0,0 +1,64 @@
+import { ReactNode } from "react";
+import { create } from "zustand";
+
+type PopupState = {
+  content: ReactNode | null;
+  isOpen: boolean;
+  cancleCallback: () => void | undefined;
+  confirmCallback: () => void | undefined;
+};
+
+type PopupActions = {
+  openPopup: (
+    content: ReactNode,
+    cancleCallback?: () => void | undefined,
+    confirmCallback?: () => void | undefined
+  ) => void;
+  closePopup: (cancleCallback?: () => void) => void;
+};
+
+const usePopupModalStore = create<PopupState & PopupActions>((set) => ({
+  content: null,
+  isOpen: false,
+  cancleCallback: () => undefined,
+  confirmCallback: () => undefined,
+
+  openPopup: (content: ReactNode, cancleCallback?: () => void, confirmCallback?: () => void) => {
+    set({ content, isOpen: true, cancleCallback, confirmCallback });
+  },
+  closePopup: () => {
+    set({
+      content: null,
+      isOpen: false,
+      cancleCallback: undefined,
+      confirmCallback: undefined,
+    });
+  },
+}));
+
+/**
+ * 팝업 모달의 상태를 반환합니다.
+ * @returns content - 팝업에 띄울 내용
+ * @returns isOpen - 팝업이 열려 있는지 여부
+ * @returns cancleCallback - 팝업을 닫을 때 추가적인 작업이 필요하다면 이 콜백을 사용합니다. 콜백핸들러 설정은 usePopupActon()의 openPopup에서 처리함
+ * @returns confirmCallback - 확인 버튼을 누를 때 추가적인 작업이 필요하다면 이 콜백을 사용합니다. 콜백핸들러 설정은 usePopupActon()의 openPopup에서 처리함
+ */
+export const usePopupState = () => {
+  const content = usePopupModalStore((state) => state.content);
+  const isOpen = usePopupModalStore((state) => state.isOpen);
+  const closeCallback = usePopupModalStore((state) => state.cancleCallback);
+  const confirmCallback = usePopupModalStore((state) => state.confirmCallback);
+
+  return { content, isOpen, closeCallback, confirmCallback };
+};
+
+/**
+ * 팝업 모달의 액션을 반환합니다.
+ * @returns openPopup - 팝업을 엽니다. 필요한경우 closeCallback,confirmCallback을 설정할 수 있습니다.
+ * @returns closePopup - 팝업을 닫습니다
+ */
+export const usePopupActon = () => {
+  const openPopup = usePopupModalStore((state) => state.openPopup);
+  const closePopup = usePopupModalStore((state) => state.closePopup);
+  return { openPopup, closePopup };
+};
