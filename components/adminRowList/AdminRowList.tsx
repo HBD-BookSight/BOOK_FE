1a7168ebaf014a1428ddbcb53fa40f387cc3c9c8
yugyusang <yugyusang12@gmail.com>
2025-03-06 17:45:24 +0900

    feat: 어드민 페이지검색 결과를 보여주는 컴포넌트 AdminRowList,AdminRowListItem 구현

---
+++ components/adminRowList/AdminRowList.tsx
@@ -0,0 +1,46 @@
+"use client";
+import { usePageData } from "@/context/PageDataProvider";
+import React, { HTMLAttributes } from "react";
+import MemoizedAdminRowListItem from "./AdminRowListItem";
+
+type Props = {
+  keys: string[];
+  className?: string;
+} & HTMLAttributes<HTMLDivElement>;
+const AdminRowList = <T,>({ className, keys, ...props }: Readonly<Props>) => {
+  const { data, setSelectRow, selectRow } = usePageData<T>();
+
+  return (
+    <div className={`relative flex flex-1 flex-col overflow-auto ${className || ""}`} {...props}>
+      <div className="absolute size-full">
+        <div className="sticky top-0 flex w-fit flex-row gap-2 border-b-[1px] bg-white pl-6">
+          <p className="w-24 overflow-hidden text-ellipsis font-bold"></p>
+          {keys.map((key, index) => {
+            return (
+              <p key={index} className="w-24 overflow-hidden text-ellipsis font-bold">
+                {key}
+              </p>
+            );
+          })}
+        </div>
+        {data instanceof Array &&
+          data.map((item, rowIndex) => {
+            if (typeof item === "object") {
+              return (
+                <MemoizedAdminRowListItem<T>
+                  key={rowIndex}
+                  item={item}
+                  keys={keys}
+                  rowIndex={rowIndex}
+                  isSelect={selectRow === rowIndex}
+                  setSelectRow={setSelectRow}
+                />
+              );
+            }
+          })}
+      </div>
+    </div>
+  );
+};
+
+export default AdminRowList;
---
+++ components/adminRowList/AdminRowListItem.tsx
@@ -0,0 +1,47 @@
+"use client";
+import React, { HTMLAttributes, memo, NamedExoticComponent } from "react";
+import CommonRadioButton from "../common/CommonRadioButton";
+
+/**
+ * ListItem 특성상 많은 수가 렌더링되고 contextAPI가 부모에서 참조되므로 스토어 상태 수정시
+ * 부모는 무조건 렌더링되고 자식도 그에따라 렌더링되므로 쓸모없는 재렌더링을 방지하기위해 memo로 렌더링
+ * 캐싱이 필요없다면 AdminRowListItem를 사용할 것
+ */
+type Props<T> = {
+  className?: string;
+  item: T;
+  keys: string[];
+  rowIndex: number;
+  isSelect: boolean;
+  setSelectRow: (rowIndex: number) => void;
+} & HTMLAttributes<HTMLDivElement>;
+export const AdminRowListItem = <T,>({
+  className,
+  item,
+  keys,
+  rowIndex,
+  isSelect,
+  setSelectRow,
+}: Readonly<Props<T>>) => {
+  return (
+    <label
+      className={`relative flex w-fit flex-row items-center gap-2 ${isSelect ? "bg-[#F8F8FE]" : ""} ${className || ""}`}
+      onClick={() => setSelectRow(rowIndex)}
+    >
+      <CommonRadioButton name="row" isSelected={isSelect} />
+      <p className="w-24 overflow-hidden text-ellipsis">{rowIndex}</p>
+      {keys.map((key: string, index: number) => (
+        <p key={index} className="w-24 overflow-hidden text-ellipsis">
+          {item[key as keyof T] !== undefined ? String(item[key as keyof T]) : ""}
+        </p>
+      ))}
+    </label>
+  );
+};
+
+const MemoizedAdminRowListItem = <T,>(props: Props<T>) => {
+  const WrappedComponent = memo(AdminRowListItem) as NamedExoticComponent<Props<T>>;
+  return <WrappedComponent {...props} />;
+};
+
+export default MemoizedAdminRowListItem;
