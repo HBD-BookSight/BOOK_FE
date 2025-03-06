1e631b35fb470ba69ee332a7cb04fcc385de7b55
yugyusang <yugyusang12@gmail.com>
2025-03-06 17:46:34 +0900

    style: 스타일 수정 및 사용하지 않는 props 제거

--- components/common/CommonToggleSwitch.tsx
+++ components/common/CommonToggleSwitch.tsx
@@ -2,13 +2,12 @@ import React, { HTMLAttributes } from "react";
 
 type Props = {
   className?: string;
-  htmlFor?: string;
 } & HTMLAttributes<HTMLLabelElement>;
 const CommonToggleSwitch = ({ className, ...props }: Readonly<Props>) => {
   return (
     <label className={`relative flex cursor-pointer items-center ${className || ""}`} {...props}>
       <input type="checkbox" className="peer sr-only" />
-      <div className="peer h-6 w-10 rounded-full bg-gray-200 after:absolute after:left-[4px] after:top-[2px] after:size-4 after:rounded-full after:bg-white after:transition-all peer-checked:bg-[var(--highlight-color)] peer-checked:after:translate-x-full peer-focus:ring-2" />
+      <div className="peer h-6 w-10 rounded-full bg-gray-200 after:absolute after:left-[4px] after:top-[2px] after:size-4 after:rounded-full after:bg-white after:transition-all peer-checked:bg-[var(--highlight-color)] peer-checked:after:translate-x-full" />
     </label>
   );
 };
