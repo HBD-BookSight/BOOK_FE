4fafe98fced625ca1341a0ae73a5e5eebc4b3939
yugyusang <yugyusang12@gmail.com>
2025-03-12 19:11:47 +0900

    feat: 공유버튼 기능 구현

--- app/(client)/(backButtonHeader)/book/[isbn]/BookHeaderHelper.tsx
+++ app/(client)/(backButtonHeader)/book/[isbn]/BookHeaderHelper.tsx
@@ -3,35 +3,51 @@ import { useBackHeader } from "@/context/backHeaderStore";
 import { useEffect, useState } from "react";
 import ShareIcon from "@/public/icons/shareIcon.svg";
 import KakaoInfoTip from "@/components/kakaoInfoTip/KakaoInfoTip";
+import { Book } from "@/types/api";
 
-type Props = { bookName?: string; isbn: number; hasData: boolean };
-const BookHeaderHelper = ({ bookName, hasData, isbn }: Readonly<Props>) => {
+type Props = { isbn: number; bookData: Book | undefined };
+const BookHeaderHelper = ({ isbn, bookData }: Readonly<Props>) => {
   const { setTitle, setEtcButton } = useBackHeader();
-  const [bookNameState, setBookNameState] = useState(bookName);
+  const [bookNameState, setBookNameState] = useState(bookData?.title || "");
   useEffect(() => {
     //카카오 검색데이터 없을시 세션 스토리지에서 제목을 가져옴
-    if (!bookName) {
+    if (!bookData?.title) {
       const bookName = sessionStorage.getItem(isbn.toString());
       if (bookName) {
         setBookNameState(JSON.parse(bookName).TITLE_NM);
       }
     }
-  }, [bookName, isbn]);
+  }, [bookData, isbn]);
 
   useEffect(() => {
     if (typeof bookNameState !== "string") return;
     setTitle(
       <div className="flex size-fit min-w-0 max-w-[50vw] flex-row items-center justify-start">
         <span className="truncate">{bookNameState}</span>
-        {hasData && <KakaoInfoTip />}
+        {bookData?.isbn && <KakaoInfoTip />}
       </div>
     );
-    setEtcButton(<ShareIcon className="text-[var(--sub-color)]" />);
+
+    const sharedHandler = () => {
+      if (navigator.share) {
+        navigator
+          .share({
+            title: "HBD 책 소개",
+            text: bookNameState,
+            url: window.location.href,
+          })
+          .then(() => console.log("공유 성공"))
+          .catch((error) => console.log("공유 실패", error));
+      } else {
+        alert("죄송합니다 현재 브라우저가 Web Share API를 지원하지 않습니다.");
+      }
+    };
+    setEtcButton(<ShareIcon className="text-[var(--sub-color)]" onClick={sharedHandler} />);
     return () => {
       setTitle("");
       setEtcButton(null);
     };
-  }, [setTitle, setEtcButton, bookNameState, hasData]);
+  }, [setTitle, setEtcButton, bookNameState, bookData]);
   return null;
 };
 
