170055b803156d70bb93d9c431f11ff31c39e797
yugyusang <yugyusang12@gmail.com>
2025-03-12 21:31:10 +0900

    refactor: handleFetchKaKaoData통신이 완료되기를 기다리지 않고 하위요소를 렌더링하도록 fetchKaKaoBookSuspenseData를 구현 및 적용

--- app/(client)/(backButtonHeader)/book/[isbn]/page.tsx
+++ app/(client)/(backButtonHeader)/book/[isbn]/page.tsx
@@ -1,23 +1,56 @@
-import React from "react";
+import React, { Suspense } from "react";
 import BookHeaderHelper from "./BookHeaderHelper";
 import BookDetail from "@/components/bookDetail/BookDetail";
 import { handleFetchKaKaoData } from "@/function/common";
+import LoadingSpinner from "@/components/common/LoadingSpinner";
+import { KaKaoBookResponse } from "@/types/api";
+
+export type SuspenseResource = {
+  read: () => KaKaoBookResponse;
+};
+
 const BookDetailpage = async ({ params }: { params: Promise<{ isbn?: number }> }) => {
   const { isbn } = await params;
-  const result = await handleFetchKaKaoData(isbn?.toString(), "isbn");
+  const suspenseResource: SuspenseResource = fetchKaKaoBookSuspenseData(isbn);
 
   return (
     <>
-      <BookHeaderHelper
-        bookName={result?.documents?.[0]?.title}
-        hasData={(result?.documents && result?.documents.length > 0) || false}
-        isbn={isbn!}
-      />
+      <BookHeaderHelperContainer suspenseResource={suspenseResource} isbn={isbn} />
       <main className="flex size-full flex-1 items-center gap-8">
-        <BookDetail book={result} isbn={isbn!} />
+        <Suspense fallback={<LoadingSpinner className="h-[80vw] w-full" />}>
+          <BookDetail suspenseResource={suspenseResource} isbn={isbn!} />
+        </Suspense>
       </main>
     </>
   );
 };
 
 export default BookDetailpage;
+
+/**
+ * handleFetchKaKaoData 통신이 끝날때까지 렌더링을 중지하면
+ * 멈춘것처럼 이전화면에서 대기 해야하므로 통신을 Promise로 저장해서 전달
+ */
+const fetchKaKaoBookSuspenseData = (isbn?: number) => {
+  let data: KaKaoBookResponse | undefined;
+  let promise: Promise<KaKaoBookResponse | undefined>;
+  return {
+    read: () => {
+      if (data) return data;
+      if (!promise) promise = handleFetchKaKaoData(isbn?.toString(), "isbn").then((result) => (data = result));
+      throw promise;
+    },
+  };
+};
+
+//클라이언트 컴포넌트인 BookHeaderHelper에게 suspenseResource를 전달하기위한 컨테이너
+const BookHeaderHelperContainer = ({
+  suspenseResource,
+  isbn,
+}: {
+  suspenseResource: SuspenseResource;
+  isbn?: number;
+}) => {
+  const resource = suspenseResource.read().documents[0];
+  return <BookHeaderHelper bookData={resource} isbn={isbn!} />;
+};
