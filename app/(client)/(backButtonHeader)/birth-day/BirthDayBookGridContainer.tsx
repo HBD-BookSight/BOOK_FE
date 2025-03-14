cb6093b842ffe67a3d778f00306ad7307e31fb7c
yugyusang <yugyusang12@gmail.com>
2025-03-14 16:00:56 +0900

    feat: BirthDayBookGridContainer 무한스크롤 구현

--- app/(client)/(backButtonHeader)/birth-day/BirthDayBookGridContainer.tsx
+++ app/(client)/(backButtonHeader)/birth-day/BirthDayBookGridContainer.tsx
@@ -1,28 +1,51 @@
 "use client";
-import BirthDayBookGrid from "@/components/bookGrid/BirthDayBookGrid";
+import BookGrid from "@/components/bookGrid/BookGrid";
+import LoadingSpinner from "@/components/common/LoadingSpinner";
 import { CSVBook } from "@/types/api";
 import { useInfiniteQuery } from "@tanstack/react-query";
+import { useEffect, useRef } from "react";
 
 const BirthDayBookGridContainer = () => {
-  const { data, fetchNextPage } = useInfiniteQuery({
+  const nextPageButtonRef = useRef<HTMLButtonElement>(null);
+  const { data, fetchNextPage, hasNextPage, isLoading } = useInfiniteQuery({
     queryKey: ["birthDayBook", new Date().getUTCDate()], //UTC날짜를 기준으로 쿼리키 유효
     queryFn: fetchInfiniteCsvData,
-
     initialPageParam: 0,
     getNextPageParam: (lastPage) => lastPage.nextCursor,
     staleTime: 24 * 60 * 60 * 1000,
   });
 
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      const target = entries[0];
+      if (target.isIntersecting && hasNextPage) {
+        fetchNextPage();
+      }
+    });
+    if (nextPageButtonRef.current) {
+      observer.observe(nextPageButtonRef.current);
+    }
+    return () => {
+      observer.disconnect();
+    };
+  }, [fetchNextPage, hasNextPage]);
+
   return (
     <>
-      {data?.pages.map((page, index) => (
-        <BirthDayBookGrid
-          key={index}
-          books={page.data || []}
-          className={`${data?.pages.length > 0 && "py-0 pt-[10px]"}`}
-        />
-      ))}
-      <button onClick={() => fetchNextPage()}>더보기</button>
+      {!isLoading ? (
+        <>
+          {data?.pages.map((page, index) => (
+            <BookGrid key={index} books={page.data || []} className={`${data?.pages.length > 0 && "py-0 pt-[10px]"}`} />
+          ))}
+          {hasNextPage && (
+            <button onClick={() => fetchNextPage()} ref={nextPageButtonRef}>
+              더보기
+            </button>
+          )}
+        </>
+      ) : (
+        <LoadingSpinner className="h-[50vh] w-full" />
+      )}
     </>
   );
 };
