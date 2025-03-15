1f48907cfc89d85627ee52536d0e09e164a6f419
yugyusang <yugyusang12@gmail.com>
2025-03-16 01:39:47 +0900

    feat: 책 상세페이지 유효기간 설정

--- app/(client)/(backButtonHeader)/book/[isbn]/page.tsx
+++ app/(client)/(backButtonHeader)/book/[isbn]/page.tsx
@@ -5,6 +5,8 @@ import { handleFetchKaKaoData } from "@/function/common";
 import LoadingSpinner from "@/components/common/LoadingSpinner";
 import { KaKaoBookResponse } from "@/types/api";
 
+export const revalidate = 24 * 60 * 60; // 캐시를 너무 오래 저장하면 더 중요한 캐시 정보(getBirthdayBook 반환값 같은거)가 제거될 수 있으므로 생일도서가 변경되는 24시간만 유효하도록 설정함
+
 const BookDetailpage = async ({ params }: { params: Promise<{ isbn?: number }> }) => {
   const { isbn } = await params;
   const suspenseResource = fetchKaKaoBookSuspense(isbn);
