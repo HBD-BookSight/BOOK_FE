5dd7e163bf6d3240710c72b16a5dbf9e14b7779d
yugyusang <yugyusang12@gmail.com>
2025-03-15 15:59:59 +0900

    feat: api/refresh에서 revalidatePath대신 함수만 다시 실핼되도록 revalidateTag로 변경 및 웜업 요청을 보내도록 기능 추가

--- app/api/refresh/route.ts
+++ app/api/refresh/route.ts
@@ -1,11 +1,21 @@
-import { revalidatePath } from "next/cache";
+import { revalidateTag } from "next/cache";
 import { NextResponse } from "next/server";
 
-// 루트페이지 서버컴포넌트를 재 렌더링 시키는 API
+// 특정 태그를 재검증 시키는 크론잡용 API
 export async function GET(req: Request) {
   if (req.headers.get("Authorization") !== `Bearer ${process.env.CRON_SECRET}`) {
-    return NextResponse.json({ message: "Unauthorized." }, { status: 401 });
+    return NextResponse.json({ message: "인증되지 않은 요청" }, { status: 401 });
   }
-  revalidatePath("/");
-  return NextResponse.json({ message: "Revalidated." });
+  try {
+    // 태그 재검증
+    revalidateTag("birth-day-book-data");
+    // 페이지 워밍업 요청
+    const baseUrl = process.env.AUTH0_BASE_URL || `https://${process.env.VERCEL_URL}`;
+    await fetch(baseUrl);
+
+    NextResponse.json({ message: "재검증 및 워밍업 완료" });
+  } catch (error) {
+    const errMessage = error instanceof Error ? error.message : "오류 발생";
+    NextResponse.json({ message: "오류 발생", error: errMessage }, { status: 500 });
+  }
 }
