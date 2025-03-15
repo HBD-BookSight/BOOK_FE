3f10debf1e0518cd87cfd98bef563f6700435311
yugyusang <yugyusang12@gmail.com>
2025-03-16 02:01:50 +0900

    fix: 페이지 유효성 변수 revalidate가 숫자 또는 상수를 받도록 수정

--- app/(client)/(backButtonHeader)/book/[isbn]/page.tsx
+++ app/(client)/(backButtonHeader)/book/[isbn]/page.tsx
@@ -5,7 +5,8 @@ import { handleFetchKaKaoData } from "@/function/common";
 import LoadingSpinner from "@/components/common/LoadingSpinner";
 import { KaKaoBookResponse } from "@/types/api";
 
-export const revalidate = 24 * 60 * 60; // 캐시를 너무 오래 저장하면 더 중요한 캐시 정보(getBirthdayBook 반환값 같은거)가 제거될 수 있으므로 생일도서가 변경되는 24시간만 유효하도록 설정함
+const ONE_DAY_IN_SECONDS = 24 * 60 * 60;
+export const revalidate = ONE_DAY_IN_SECONDS; // 캐시를 너무 오래 저장하면 더 중요한 캐시 정보(getBirthdayBook 함수처럼 무거운거)가 제거될 수 있으므로 생일도서가 변경되는 24시간만 유효하도록 설정함
 
 const BookDetailpage = async ({ params }: { params: Promise<{ isbn?: number }> }) => {
   const { isbn } = await params;
