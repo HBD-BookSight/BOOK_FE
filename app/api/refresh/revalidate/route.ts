42b5887ca1d44442732440b667fa190beb3f3d9e
yugyusang <yugyusang12@gmail.com>
2025-03-15 19:25:56 +0900

    fix: getBirthdayBook정보를 미리 캐싱할때 동일한 라우터에서 실행시 버셀이 캐시 무효화를 인식하지못하는 문제 수정

---
+++ app/api/refresh/revalidate/route.ts
@@ -0,0 +1,34 @@
+import { revalidateTag } from "next/cache";
+import getConfig from "next/config";
+import { NextResponse } from "next/server";
+
+// 새 정보를 얻도록 캐시태그를 무효화시키는 cron job용 api
+export async function GET(req: Request) {
+  if (req.headers.get("Authorization") !== `Bearer ${process.env.CRON_SECRET}`) {
+    console.log("인증되지 않은 요청");
+    return NextResponse.json({ message: "인증되지 않은 요청" }, { status: 401 });
+  }
+  try {
+    // 태그 무효화
+    revalidateTag("birth-day-book-data");
+    console.log("태그 무효화 성공");
+    await new Promise((resolve) => setTimeout(resolve, 1000)); // 1초 대기 서버가 인식하는데 시간이 필요할지 모르므로
+    const { publicRuntimeConfig } = getConfig();
+    const baseUrl = publicRuntimeConfig.VERCEL_PROJECT_PRODUCTION_URL;
+    const warmupResponse = await fetch(`${baseUrl}/api/refresh/warmup`, {
+      headers: {
+        Authorization: `Bearer ${process.env.CRON_SECRET}`,
+      },
+    });
+    if (!warmupResponse.ok) {
+      throw new Error("워밍업 요청 실패");
+    }
+    const warmupResult = await warmupResponse.json();
+    console.log("워밍업 완료, 태그 무효화 성공", warmupResult);
+    return NextResponse.json({ message: "워밍업 완료, 태그 무효화 성공" });
+  } catch (error) {
+    const errMessage = error instanceof Error ? error.message : "오류 발생";
+    console.log("오류 발생", errMessage);
+    return NextResponse.json({ message: "오류 발생", error: errMessage }, { status: 500 });
+  }
+}
--- app/api/refresh/warmup/route.ts
+++ app/api/refresh/warmup/route.ts
@@ -1,22 +1,14 @@
 import getBirthdayBook from "@/function/server/getBirtdayBook";
-import getConfig from "next/config";
 import { NextResponse } from "next/server";
 
-// 특정 태그를 재검증 시키는 크론잡용 API
+// revalidate API가 호출하여 getBirthdayBook를 미리 실행시켜 캐싱하는 API
 export async function GET(req: Request) {
   if (req.headers.get("Authorization") !== `Bearer ${process.env.CRON_SECRET}`) {
     console.log("인증되지 않은 요청");
     return NextResponse.json({ message: "인증되지 않은 요청" }, { status: 401 });
   }
   try {
-    // 페이지 워밍업 요청
-    const { publicRuntimeConfig } = getConfig();
-    const baseUrl = publicRuntimeConfig.VERCEL_PROJECT_PRODUCTION_URL;
-    if (!baseUrl) throw new Error("VERCEL_PROJECT_PRODUCTION_URL이 버셀에서 주입되지 않았습니다");
-    await fetch(baseUrl, { cache: "no-cache" });
     await getBirthdayBook();
-
-    console.log("재검증 및 워밍업 완료");
     return NextResponse.json({ message: "재검증 및 워밍업 완료" });
   } catch (error) {
     const errMessage = error instanceof Error ? error.message : "오류 발생";
