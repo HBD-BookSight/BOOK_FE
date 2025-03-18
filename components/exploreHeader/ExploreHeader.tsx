128031dd62c7450a916a98a0bf3c592ab2c78642
yugyusang <yugyusang12@gmail.com>
2025-03-18 23:46:39 +0900

    feat: explore 라우터용 헤더 구현

---
+++ components/exploreHeader/ExploreHeader.tsx
@@ -0,0 +1,53 @@
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+
+const PUBLISHER = /\/publisher$/;
+const AUTHOR = /\/author$/;
+const READER = /\/reader$/;
+const ExploreHeader = () => {
+  const pathname = usePathname();
+  return (
+    <header className="sticky top-0 flex w-full flex-col">
+      <div className="scrollbar-hide flex flex-row items-center justify-start gap-4 overflow-x-auto py-6 pl-5 text-xl font-bold">
+        <Link href={"./publisher"} className="shrink-0">
+          <p className={`p-2 ${!PUBLISHER.test(pathname) && "text-[var(--sub-color)]"}`}>출판사</p>
+          {PUBLISHER.test(pathname) && (
+            <motion.div
+              className="pointer-events-none relative size-full border-0 border-b-2 border-black"
+              layoutId="exploreHighlight"
+              initial={false}
+              transition={{ type: "spring", stiffness: 500, damping: 30 }}
+            />
+          )}
+        </Link>
+        <Link href={"./author"} className="shrink-0">
+          <p className={`p-2 ${!AUTHOR.test(pathname) && "text-[var(--sub-color)]"}`}>작가</p>
+          {AUTHOR.test(pathname) && (
+            <motion.div
+              className="pointer-events-none relative size-full border-0 border-b-2 border-black"
+              layoutId="exploreHighlight"
+              initial={false}
+              transition={{ type: "spring", stiffness: 500, damping: 30 }}
+            />
+          )}
+        </Link>
+        <Link href={"./reader"} className="shrink-0">
+          <p className={`p-2 ${!READER.test(pathname) && "text-[var(--sub-color)]"}`}>독자</p>
+          {READER.test(pathname) && (
+            <motion.div
+              className="pointer-events-none relative size-full border-0 border-b-2 border-black"
+              layoutId="exploreHighlight"
+              initial={false}
+              transition={{ type: "spring", stiffness: 500, damping: 30 }}
+            />
+          )}
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default ExploreHeader;
