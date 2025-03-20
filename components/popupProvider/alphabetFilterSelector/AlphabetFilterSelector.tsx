61bf904796694ee91a578a2ed29262ac4bde595e
yugyusang <yugyusang12@gmail.com>
2025-03-20 18:04:17 +0900

    feat: 알파벳 필터 셀렉터 구현 및 모달 생성 적용

---
+++ components/popupProvider/alphabetFilterSelector/AlphabetFilterSelector.tsx
@@ -0,0 +1,82 @@
+import CommonPillButton from "@/components/common/CommonPillButton";
+import { usePopupState } from "@/context/popupStore";
+import React, { HTMLAttributes, useEffect, useRef, useState } from "react";
+
+type Props = {
+  className?: string;
+} & HTMLAttributes<HTMLDivElement>;
+const AlphabetFilterSelector = ({ className, ...props }: Readonly<Props>) => {
+  const { closeCallback, confirmCallback } = usePopupState();
+  const [current, setCurrent] = useState<string>("A-Z");
+  const ref = useRef<HTMLDivElement[]>([]);
+  const containerRef = useRef<HTMLUListElement>(null);
+  const setFilterHandler = (alphabet: string) => {
+    closeCallback();
+    if (confirmCallback) {
+      confirmCallback(alphabet);
+    }
+  };
+
+  useEffect(() => {
+    const intersectionObserver = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add("!text-[var(--highlight-color)]");
+            if (entry.target.textContent) setCurrent(entry.target.textContent);
+          } else {
+            entry.target.classList.remove("!text-[var(--highlight-color)]");
+          }
+        });
+      },
+      {
+        root: containerRef.current,
+        rootMargin: "-45% 0px -45% 0px",
+      }
+    );
+    ref.current.forEach((element) => {
+      intersectionObserver.observe(element);
+    });
+    return () => {
+      intersectionObserver.disconnect();
+    };
+  }, []);
+
+  return (
+    <div
+      className={`flex size-fit w-full flex-col rounded-t-[40px] bg-white p-6 shadow-[0_0_20px_0_rgba(0,0,0,0.12)] ${
+        className || ""
+      }`}
+      {...props}
+    >
+      <ul
+        className="flex max-h-[30vh] snap-y snap-mandatory flex-col gap-[5vh] overflow-y-scroll py-[10vh]"
+        ref={containerRef}
+      >
+        {alphabetArray.map((alphabet, index) => {
+          return (
+            <div
+              key={index}
+              className="flex snap-center items-center justify-center text-2xl text-[#DBE1F1]"
+              ref={(element) => {
+                if (element) ref.current[index] = element;
+              }}
+            >
+              <p>{alphabet}</p>
+            </div>
+          );
+        })}
+      </ul>
+      <CommonPillButton
+        onClick={() => setFilterHandler(current)}
+        className="h-20 border-none bg-[var(--sub-highlight-color)] !text-lg font-bold"
+      >
+        확인
+      </CommonPillButton>
+    </div>
+  );
+};
+
+export default AlphabetFilterSelector;
+
+const alphabetArray = ["A-Z", ...Array.from({ length: 26 }, (_, i) => String.fromCharCode(97 + i))];
--- components/publisherFilterController/PublisherFilterController.tsx
+++ components/publisherFilterController/PublisherFilterController.tsx
@@ -3,20 +3,33 @@ import React from "react";
 import CommonPillButton from "../common/CommonPillButton";
 import TriangleArrow from "@/public/icons/triangleArrowIcon.svg";
 import { ContentsFilterType, usePublisherPageFilterControllerData } from "@/context/PublisherPageDataProvider";
-import { ModalType, usePopupActon } from "@/context/popupStore";
+import { ModalType, usePopupAction } from "@/context/popupStore";
 import BottomSheetModal from "../popupProvider/BottomSheetModal";
-import ExploreOrderSelector from "../popupProvider/exploreOrderSelector/ExploreOrderSelector";
+import AlphabetFilterSelector from "../popupProvider/alphabetFilterSelector/AlphabetFilterSelector";
 
 const PublisherFilterController = () => {
-  const { contentsfilter, setContentsFilter } = usePublisherPageFilterControllerData();
-  const { openPopup, closePopup } = usePopupActon();
+  const { contentsfilter, setContentsFilter, alphabetFilter, setAlphabetFilter } =
+    usePublisherPageFilterControllerData();
+  const { openPopup, closePopup } = usePopupAction();
+
+  const bottomSheetOpenHander = () => {
+    openPopup<string>(
+      <BottomSheetModal>
+        <AlphabetFilterSelector />
+      </BottomSheetModal>,
+      closePopup,
+      setAlphabetFilter,
+      ModalType.BOTTOMSHEET
+    );
+  };
+
   return (
     <header className="sticky top-0 flex w-full">
       <div className="scrollbar-hide flex flex-row items-center justify-start gap-2 overflow-x-auto pl-5 text-xl">
         <CommonPillButton
-          className={`!size-fit shrink-0  px-4 ${
+          className={`!size-fit shrink-0 px-4 transition-colors ${
             contentsfilter?.includes(ContentsFilterType.NEWS_LETTER)
-              ? "border-none bg-gradient-to-r from-[#B0B5DF] to-[#EEDBE0] font-semibold text-white"
+              ? "border-none bg-gradient-to-r from-[#B0B5DF] to-[#EEDBE0] !font-semibold text-white"
               : "border-gray-200 px-4 text-[var(--sub-color)]"
           }`}
           onClick={() =>
@@ -30,9 +43,9 @@ const PublisherFilterController = () => {
           뉴스레터
         </CommonPillButton>
         <CommonPillButton
-          className={`!size-fit shrink-0  px-4 ${
+          className={`!size-fit shrink-0 px-4 transition-colors ${
             contentsfilter?.includes(ContentsFilterType.PLAY_LIST)
-              ? "border-none bg-gradient-to-r from-[#B0B5DF] to-[#EEDBE0] font-semibold text-white"
+              ? "border-none bg-gradient-to-r from-[#B0B5DF] to-[#EEDBE0] !font-semibold text-white"
               : "border-gray-200 px-4 text-[var(--sub-color)]"
           }`}
           onClick={() =>
@@ -47,19 +60,10 @@ const PublisherFilterController = () => {
         </CommonPillButton>
         <div className="h-3/5 w-0 border-r"></div>
         <CommonPillButton
-          className="!size-fit shrink-0 border-gray-200 px-4 text-[var(--sub-color)]"
-          onClick={() =>
-            openPopup(
-              <BottomSheetModal>
-                <ExploreOrderSelector></ExploreOrderSelector>
-              </BottomSheetModal>,
-              closePopup,
-              closePopup,
-              ModalType.BOTTOMSHEET
-            )
-          }
+          className="!size-fit shrink-0 border-gray-200 from-[#B0B5DF] to-[#EEDBE0] px-4 text-[var(--sub-color)] transition-colors focus:bg-gradient-to-r focus:font-semibold focus:text-white"
+          onClick={bottomSheetOpenHander}
         >
-          출판사명 A-Z
+          출판사명 {alphabetFilter}
           <TriangleArrow className={`flex size-4 rotate-180 items-center justify-center transition duration-300`} />
         </CommonPillButton>
       </div>
