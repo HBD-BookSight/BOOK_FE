e32f0020450fb334cfe4cca97aab53f691c60c09
yugyusang <yugyusang12@gmail.com>
2025-03-21 22:52:27 +0900

    feat: MainScheduleSlide에 schedule라우터 이동 버튼 추가

--- components/mainScheduleSlide/MainScheduleSlide.tsx
+++ components/mainScheduleSlide/MainScheduleSlide.tsx
@@ -1,5 +1,7 @@
 import { HTMLAttributes } from "react";
 import MainScheduleSlideContainer from "./MainScheduleSlideContainer";
+import BackArrowIcon from "@/public/icons/backArrowIcon.svg";
+import Link from "next/link";
 
 type Props = { className?: string } & HTMLAttributes<HTMLDivElement>;
 const MainScheduleSlide = ({ className, ...props }: Readonly<Props>) => {
@@ -8,8 +10,19 @@ const MainScheduleSlide = ({ className, ...props }: Readonly<Props>) => {
       className={`relative flex size-full flex-col pl-[var(--client-layout-margin)] ${className || ""}`}
       {...props}
     >
-      <h2 className="mb-1 text-lg font-bold">다가오는 일정</h2>
-      <p className="mb-4 text-sm text-[var(--sub-color)]">책을 좋아하는 당신을 위한 이벤트</p>
+      <div className="relative flex size-full flex-row items-center justify-between pr-[var(--client-layout-margin)]">
+        <div>
+          <h2 className="section-title mb-1">다가오는 일정</h2>
+          <p className="section-sub-title mb-4">책을 좋아하는 당신을 위한 이벤트</p>
+        </div>
+        <Link
+          className="flex size-fit items-center justify-center border-none bg-white px-4 text-[var(--sub-color)]"
+          href="/schedule"
+          scroll={false}
+        >
+          <p>view all</p> <BackArrowIcon className="size-5 rotate-180" />
+        </Link>
+      </div>
       <MainScheduleSlideContainer />
     </section>
   );
