dd855ee719b16c4c2ac402a1a52c99323d5ec7b1
yugyusang <yugyusang12@gmail.com>
2025-04-01 02:00:04 +0900

    feat: discovery페이지에 DiscoveryPageDataProvider,DiscoveryFilterController 적용

--- app/(client)/(backButtonHeader)/discovery/page.tsx
+++ app/(client)/(backButtonHeader)/discovery/page.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 import DiscoveryHeaderHelper from "./DiscoveryHeaderHelper";
+import DiscoveryPageDataProvider from "./DiscoveryPageDataProvider";
+import DiscoveryFilterController from "@/components/discoveryFilterController/DiscoveryFilterController";
+import DiscoveryItem from "@/components/discovery/DiscoveryItem";
 
 const discoveryPage = () => {
   return (
-    <main className="relative flex size-full flex-col items-center">
+    <main className="relative flex flex-col items-center overflow-hidden">
       <DiscoveryHeaderHelper />
+      <DiscoveryPageDataProvider initialData={{}}>
+        <DiscoveryFilterController />
+        <ul className="w-full px-[var(--client-layout-margin)]">
+          <DiscoveryDataContainer />
+        </ul>
+      </DiscoveryPageDataProvider>
     </main>
   );
 };
 
 export default discoveryPage;
+
+const DiscoveryDataContainer = () => {
+  return [...new Array(12)].map((_item, index) => <DiscoveryItem key={index} className="h-fit w-full"></DiscoveryItem>);
+};
