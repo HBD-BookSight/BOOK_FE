ca4f56765c6a0a521d913b3e92e33be08f697207
yugyusang <yugyusang12@gmail.com>
2025-04-11 16:24:09 +0900

    feat: EtcSheet가 모바일에서 pullToRefresh가 동작하지 않도록 하고  터치이벤트에서 동작하도록 구현

--- components/popupProvider/etcSheet/EtcSheet.tsx
+++ components/popupProvider/etcSheet/EtcSheet.tsx
@@ -17,26 +17,39 @@ const EtcSheet = ({ className, children, ...props }: Readonly<Props>) => {
       setIsGrab(false);
     };
     document.addEventListener("mouseup", releaseHandler);
-    return () => document.removeEventListener("mouseup", releaseHandler);
+    document.addEventListener("touchend", releaseHandler);
+    return () => {
+      document.removeEventListener("mouseup", releaseHandler);
+      document.removeEventListener("touchend", releaseHandler);
+    };
   }, []);
 
   useEffect(() => {
-    const sheetSizeHandler = (e: MouseEvent) => {
+    const sheetSizeHandler = (e: MouseEvent | TouchEvent) => {
       if (isGrab && ref.current) {
-        ref.current.style.height = Math.min(initialHeight, ref.current.offsetHeight - (e.movementY || 0)) + "px";
-        if (isGrab && ref.current && ref.current.offsetHeight <= 80) {
+        if (e instanceof MouseEvent) {
+          ref.current.style.height = Math.min(initialHeight, ref.current.offsetHeight - (e.movementY || 0)) + "px";
+        }
+        if (e instanceof TouchEvent) {
+          e.preventDefault();
+          ref.current.style.height =
+            Math.min(initialHeight, window.innerHeight - (e.touches[0].clientY - 24 || 0)) + "px"; // 24는 시트헤더 위치임
+          console.log(ref.current.style.height, window.innerHeight - (e.touches[0].clientY || 0));
+        }
+        if (isGrab && ref.current && ref.current.offsetHeight <= 60) {
           closePopup();
         }
       }
     };
     document.addEventListener("mousemove", sheetSizeHandler);
+    document.addEventListener("touchmove", sheetSizeHandler, { passive: false });
     return () => document.removeEventListener("mousemove", sheetSizeHandler);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isGrab, initialHeight]);
 
   return (
     <div
-      className={`flex size-fit w-full select-none flex-col gap-6 rounded-t-[40px] bg-white p-6 font-semibold shadow-[0_0_20px_0_rgba(0,0,0,0.12)] ${
+      className={`flex size-fit w-full flex-col gap-6 rounded-t-[40px] bg-white p-6 font-semibold shadow-[0_0_20px_0_rgba(0,0,0,0.12)] ${
         className || ""
       }`}
       ref={ref}
@@ -45,6 +58,7 @@ const EtcSheet = ({ className, children, ...props }: Readonly<Props>) => {
       <div
         className="mx-auto h-4 w-16 shrink-0 cursor-grab border-t-4 border-gray-300"
         onMouseDown={() => setIsGrab((prev) => !prev)}
+        onTouchStart={() => setIsGrab((prev) => !prev)}
       ></div>
       {children}
     </div>
