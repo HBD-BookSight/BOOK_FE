b8c741e64b363011f8c8c6de0b0c6df620d5dbf8
Miinkyuuu <yng1404@gmail.com>
2025-05-11 19:00:40 +0900

    feat : fetchDailyDiscovery 적용 빈데이터 처리 UI 추가

---
+++ function/fetch/fetchDailyDiscovery.ts
@@ -0,0 +1,24 @@
+import { ContentsDto, ListResponseContentsDto } from "@/types/dto";
+
+const fetchDailyDiscovery = async (): Promise<ContentsDto[] | undefined> => {
+  try {
+    const response = await fetch(
+      process.env.NEXT_PUBLIC_BACKEND_URL + "/contents/discovery",
+      {
+        next: { revalidate: 86400, tags: ["daily-discovery"] },
+      }
+    );
+    if (response.ok) {
+      const result: ListResponseContentsDto = await response.json();
+      console.log(result);
+
+      return result.items;
+    } else {
+      throw new Error(response.statusText);
+    }
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+};
+
+export default fetchDailyDiscovery;
