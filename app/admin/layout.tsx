977bd636f9fed598a03dbd3a0169a85135f744eb
Miinkyuuu <yng1404@gmail.com>
2025-05-18 20:19:53 +0900

    feat : 내부 레이아웃구조 최상단으로 배치

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -9,11 +9,11 @@ const AdminLayout = ({
   children: ReactNode;
 }>) => {
   return (
-    <body className={`relative mx-auto flex h-full min-h-screen w-screen flex-col bg-white antialiased`}>
+    <div className={`relative mx-auto flex h-full min-h-screen w-screen flex-col bg-white antialiased`}>
       <ReactQueryDevtools initialIsOpen={false} />
       <div className="relative mx-auto flex size-full flex-1 flex-col">{children}</div>
       <PopupProvider />
-    </body>
+    </div>
   );
 };
 export default AdminLayout;
