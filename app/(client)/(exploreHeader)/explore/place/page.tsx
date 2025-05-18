120f4189a8b5d21c24efd87c2a76249a310b0832
Miinkyuuu <yng1404@gmail.com>
2025-05-18 16:58:11 +0900

    feat : 각 탐색 임시 페이지 제작

--- app/(client)/(exploreHeader)/explore/place/page.tsx
+++ app/(client)/(exploreHeader)/explore/place/page.tsx
@@ -1,5 +1,17 @@
+import Image from "next/image";
+
 const Placepage = () => {
-  return <div></div>;
+  return (
+    <div className="relative min-h-screen w-full overflow-hidden">
+      <Image
+        fill
+        src={"/images/공간.png"}
+        alt={"공간이미지"}
+        style={{ objectFit: "cover" }}
+        priority
+      />
+    </div>
+  );
 };
 
 export default Placepage;
