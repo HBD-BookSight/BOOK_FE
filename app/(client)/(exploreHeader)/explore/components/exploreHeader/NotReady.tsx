42e03e5adc61a5249504a8b99d554319e19d5feb
Miinkyuuu <yng1404@gmail.com>
2025-06-09 22:25:30 +0900

    :recycle: [refactor] : 탐색 미완성 UI 버튼과 이미지 분리

---
+++ app/(client)/(exploreHeader)/explore/components/exploreHeader/NotReady.tsx
@@ -0,0 +1,10 @@
+const NotReady = () => {
+  return (
+    <div className="backdrop-blur-blur relative z-50 flex size-full min-w-[325px] items-center justify-center rounded-[20px] bg-zinc-600/80 py-[19px] text-sm font-semibold text-white">
+      <span>기능 준비중 입니다</span>
+      <span>Opening Soon!</span>
+    </div>
+  );
+};
+
+export default NotReady;
--- app/(client)/(exploreHeader)/explore/reader/page.tsx
+++ app/(client)/(exploreHeader)/explore/reader/page.tsx
@@ -1,14 +1,18 @@
 import Image from "next/image";
+import NotReady from "../components/exploreHeader/NotReady";
 
 const Readerpage = () => {
   return (
-    <div className="relative w-full" style={{ height: "calc(100vh)" }}>
+    <div className="absolute top-0 w-full" style={{ height: "calc(100vh)" }}>
       <Image
         fill
         src="/images/독자.png"
         alt="독자이미지"
         style={{ objectFit: "cover" }}
       />
+      <div className="absolute bottom-[120px] flex w-full justify-center px-5">
+        <NotReady />
+      </div>
     </div>
   );
 };
