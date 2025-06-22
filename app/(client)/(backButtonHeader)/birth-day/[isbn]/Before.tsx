28f0a8afdce7ea50f3541565b6e9222a85da4122
Miinkyuuu <yng1404@gmail.com>
2025-06-22 16:07:56 +0900

    :recycle: [refactor] : 기존 csv 기반 상세조회 컴포넌트 분리

---
+++ app/(client)/(backButtonHeader)/birth-day/[isbn]/Before.tsx
@@ -0,0 +1,68 @@
+import LoadingSpinner from "@/components/common/LoadingSpinner";
+import { CSVBook } from "@/types/api";
+import getConfig from "next/config";
+import { Suspense } from "react";
+import BookHeaderHelper from "../../book/[isbn]/BookHeaderHelper";
+import BookDetailCsv from "@/components/bookDetail/BookDetailCsv";
+
+export const dynamic = "force-static";
+export const revalidate = 86400; // 캐시를 너무 오래 저장하면 더 중요한 캐시 정보(getBirthdayBook 함수처럼 무거운거)가 제거될 수 있으므로 생일도서가 변경되는 24시간만 유효하도록 설정함
+
+const BirthDayBookDetailpage = async ({
+  params,
+}: {
+  params: Promise<{ isbn?: number }>;
+}) => {
+  const { isbn } = await params;
+  const suspenseResource = fetchCsvBookDataSuspense(isbn);
+  return (
+    <>
+      <BookHeaderHelper bookData={undefined} isbn={isbn!} />
+      <main className="flex size-full flex-1 items-center gap-8">
+        <Suspense fallback={<LoadingSpinner className="h-[50vw] w-full" />}>
+          <BookDetailCsv<CsvSuspenseResource>
+            suspenseResource={suspenseResource}
+          />
+        </Suspense>
+      </main>
+    </>
+  );
+};
+
+export default BirthDayBookDetailpage;
+
+export type CsvSuspenseResource = {
+  read: () => CSVBook[];
+};
+const fetchCsvBookDataSuspense = (isbn?: number): CsvSuspenseResource => {
+  let data: CSVBook[] | undefined;
+  let promise: Promise<CSVBook[] | undefined>;
+  return {
+    read: () => {
+      if (data) return data;
+      if (!promise)
+        promise = fetchCsvData(isbn).then((result) => (data = result));
+      throw promise;
+    },
+  };
+};
+
+const fetchCsvData = async (isbn?: number) => {
+  if (!isbn) throw new Error("ISBN is required");
+  try {
+    const { publicRuntimeConfig } = getConfig();
+    const baseUrl = publicRuntimeConfig.VERCEL_PROJECT_PRODUCTION_URL;
+    const response = await fetch(`${baseUrl}/api/book?isbn=` + isbn, {
+      cache: "force-cache",
+      next: { revalidate: 86400, tags: [isbn.toString()] },
+    });
+    if (!response.ok) throw new Error("Failed to fetch data");
+    const data = await response.json();
+    const keys = Object.keys(data.data);
+    const parseData: CSVBook[] = keys.map((key) => data.data[key]);
+    return parseData;
+  } catch (error) {
+    console.error("Error fetching CSV data:", error);
+    throw error;
+  }
+};
---
+++ components/bookDetail/BookDetailCsv.tsx
@@ -0,0 +1,77 @@
+import { KakaoSuspenseResource } from "@/app/(client)/(backButtonHeader)/book/[isbn]/page";
+import { HTMLAttributes } from "react";
+import BookDetailImageSection from "./BookDetailImageSection";
+import BookDetailSection from "./BookDetailSection";
+import BookTitleSection from "./BookTitleSection";
+import ReadersReview from "./ReadersReview";
+import RecommandedItem from "./RecommandedItem";
+import { CsvSuspenseResource } from "@/app/(client)/(backButtonHeader)/birth-day/[isbn]/Before";
+
+type Props<T extends CsvSuspenseResource | KakaoSuspenseResource> = {
+  className?: string;
+  suspenseResource: T;
+} & HTMLAttributes<HTMLDivElement>;
+const BookDetailCsv = async <
+  T extends CsvSuspenseResource | KakaoSuspenseResource
+>({
+  className,
+  suspenseResource,
+  ...props
+}: Readonly<Props<T>>) => {
+  const result = suspenseResource.read();
+  const bookData = Array.isArray(result)
+    ? {
+        thumbnail: result[0].IMAGE_URL,
+        datetime: result[0].TWO_PBLICTE_DE,
+        title: result[0].TITLE_NM,
+        url: "",
+        authors: result[0].AUTHR_NM.split(", "),
+        publisher: result[0].PUBLISHER_NM,
+        contents: result[0].BOOK_INTRCN_CN,
+      }
+    : result.documents[0];
+
+  const kstBirthDay = new Date(
+    new Date(bookData?.datetime).getTime() + 9 * 60 * 60 * 1000
+  );
+
+  return (
+    <div
+      className={`relative flex size-full flex-col gap-7 px-[var(--client-layout-margin)] ${
+        className || ""
+      }`}
+      {...props}
+    >
+      <BookDetailImageSection
+        imageUrl={bookData?.thumbnail}
+        birthDay={kstBirthDay}
+        className="mt-6"
+      />
+      <BookTitleSection
+        bookName={bookData?.title || "책 이름(정보 미제공)"}
+        birthDayDate={kstBirthDay}
+        url={bookData?.url}
+        author={bookData?.authors.join(", ") || "저자 이름(정보 미제공)"}
+      />
+      <BookDetailSection
+        author={bookData?.authors.join(", ") || "저자 이름(정보 미제공)"}
+        pulisher={bookData?.publisher || "출판사(정보 미제공)"}
+        description={bookData?.contents || "책 소개(정보 미제공)"}
+        viewMoreUrl={bookData?.url}
+        className="mb-10"
+      />
+      <section>
+        <h2 className="text-lg font-bold">추천 콘텐츠</h2>
+        <RecommandedItem
+          imageUrl="https://s3-alpha-sig.figma.com/img/8d4f/4b13/868fb4811d7171eef83e5a8d8988ba13?Expires=1740960000&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=ORxF1blpY9piMcDNWkmkCr~abQC~WhkmoV7bR8-1ZScEsgdDOEC~8K-fqVZWqkV9CCAm9yq5kkVK41L-FC8CaWHMeg6-jXLoMccBB0h3L1UDZNW94CxQGXq5eaniifhL65J3yI-yzkLhNthlH7kj2vrEZSg3GNDkOdnznkIaG6VqGoOfiPwqKIsJpo0oDNGmYpaFecaKCtQwvXD43jaZXmvwL1jIXwFuNrG7wTJkvnEYNf8CkWfCTxBExvqEiqwaj94XxeOpGA1wWWAmyPVGqG1Ngm2sifZWw3XkNGnZs0FDqd25Wo0eZHpTRAG7nFIzAq81iB2WBEz~prRFDvoNnw__"
+          sourceName="유튜브"
+          sourceUrl="https://www.youtube.com/watch?v=example"
+          title="fasdfasdfasdfasdsdfasdfasdfasdfasdfasdfasdfaasdsdfasdfasdfasdfasdfasdfasdfassdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfafasd"
+        />
+      </section>
+      <ReadersReview />
+    </div>
+  );
+};
+
+export default BookDetailCsv;
